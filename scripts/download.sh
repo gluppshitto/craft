19d0a0d515ca3e8f4491485f04917478ce4e68b6
Harry Morgan <harrywm12@gmail.com>
2023-05-03 20:05:58 +0100

    Update wgets

--- scripts/download.sh
+++ scripts/download.sh
@@ -10,9 +10,8 @@ rm -rf LITV3-Serverpack-1.10.zip
 cp -a LITV3-Serverpack-1.10 ../
 rm -rf LITV3-Serverpack-1.10
 
-sudo wget -c https://github.com/gluppshitto/craft/raw/main/serverpack_config.tar.gz -O - | sudo tar -xz
-sudo wget -c https://github.com/gluppshitto/craft/raw/main/serverpack_world.tar.gz -O - | sudo tar -xz -C /world
+sudo curl -s -L https://github.com/gluppshitto/craft/raw/main/serverpack_config.tar.gz | sudo tar xvf - 
+sudo curl -s -L https://github.com/gluppshitto/craft/raw/main/serverpack_world.tar.gz | sudo tar xvf - -C /world
 
-sudo wget -c https://github.com/gluppshitto/craft/raw/main/serverpack_region_1.tar.gz -O - | sudo tar -xz -C /world/region
-sudo wget -c https://github.com/gluppshitto/craft/raw/main/serverpack_region_2.tar.gz -O - | sudo tar -xz -C /world/region
-
+sudo curl -s -L https://github.com/gluppshitto/craft/raw/main/serverpack_region_1.tar.gz| sudo tar xvf -C /world/region
+sudo curl -s -L https://github.com/gluppshitto/craft/raw/main/serverpack_region_2.tar.gz| sudo tar xvf -C /world/region
