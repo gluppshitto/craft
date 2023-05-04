1a3b413f17faa883b3f37060436974898297aa4a
Harry Morgan <harrywm12@gmail.com>
2023-05-04 15:39:12 +0100

    SUDO the java command

--- infrastructure/scripts/init.sh
+++ infrastructure/scripts/init.sh
@@ -46,4 +46,4 @@ if [ ! -f "eula.txt" ]; then
 fi
 
 # run server
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.2.0/unix_args.txt "$@"
+sudo java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.2.0/unix_args.txt "$@"
