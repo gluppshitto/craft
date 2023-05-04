30be19d6288f886e9babefc08660ddacaa93e504
Harry Morgan <harrywm12@gmail.com>
2023-05-04 22:14:26 +0100

    Update scripts

--- infrastructure/scripts/init.sh
+++ infrastructure/scripts/init.sh
@@ -11,14 +11,17 @@ sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 sudo rpm -ivh jdk-17_linux-x64_bin.rpm
 java -version
 
+# install forge and serverpack
 sudo aws s3 cp s3://glupp-metrics-storage/LITV3-Serverpack-1.10.zip ./lts.zip
 sudo unzip lts.zip
 sudo cp -a LITV3-Serverpack-1.10/. ./
 
+# grab config files
 sudo wget https://github.com/gluppshitto/craft/raw/main/assets/serverpack_config.tar.gz
 sudo tar xvf serverpack_config.tar.gz 
 sudo cp -a serverpack_config/. ./
 
+# grab world files
 if [[ $(aws s3 ls s3://glupp-metrics-storage/world/) ]]; then
     aws s3 cp s3://glupp-metrics-storage/world/ world --recursive
 else
@@ -35,9 +38,14 @@ else
     cp -a serverpack_region_2/region/. world/region/
 fi
 
+# Setup backup cron
 sudo -i
+
 touch ../etc/cron.d/backup_hourly
-echo "*/30 * * * * root aws s3 cp minecraft_server/world s3://glupp-metrics-storage/world/ --recursive" > ../etc/cron.d/backup_hourly
+echo "*/10 * * * * root /minecraft_server/backup.sh" > ../etc/cron.d/backup_hourly
+
+touch backup.sh
+echo "aws s3 cp /minecraft_server/world/ s3://glupp-metrics-storage/world/ --recursive" > backup.sh
 
 sudo sed -i s/eula=false/eula=true/g eula.txt
 
