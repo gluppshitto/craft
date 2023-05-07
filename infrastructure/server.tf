8fb1f401a9563b7de735c3b197e9fea09aeb452f
Harry Morgan <harrywm12@gmail.com>
2023-05-07 15:37:27 +0100

    Update asg

--- infrastructure/server.tf
+++ infrastructure/server.tf
@@ -17,7 +17,7 @@ resource "aws_autoscaling_group" "server_asg" {
 
   desired_capacity = 1
   max_size         = 1
-  min_size         = 1
+  min_size         = 0
 
   vpc_zone_identifier = local.private_subnets
 
